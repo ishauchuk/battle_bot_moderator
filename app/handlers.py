e5c45c94aa2b989442dc1077e4ac70fc654affda
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2022-04-09 19:27:32 +0300

    feat: added a die roll and a greeting for new participants

--- app/handlers.py
+++ app/handlers.py
@@ -1,9 +1,28 @@
 from telethon import events
+from telethon.tl.custom import Message
+from telethon.tl.types import InputMediaDice
 from app import bot
 
 
-@bot.on(events.ChatAction())
+@bot.on(events.ChatAction(
+    func=lambda e: e.is_group and e.user_added and e.user_id == bot.me.id))
 async def on_join(event: events.ChatAction.Event):
-    if event.is_group and event.user_added and event.user_id == bot.me.id:
-        await bot.send_message(event.chat.id,
-                               "Categorically welcome, gentlemens!")
+    await event.respond("Categorically welcome, gentlemen!")
+
+
+@bot.on(events.NewMessage(
+    func=lambda e: e.text.lower() == "say something"))
+async def who_are_you(event: Message):
+    await event.respond(
+        "I am a smart, handsome, moderately well-fed man in full of strength")
+
+
+@bot.on(events.NewMessage(func=lambda e: e.text.lower() == '/dice'))
+async def send_dice(event: Message):
+    await bot.send_message(event.chat.id, file=InputMediaDice(""))
+
+
+@bot.on(events.ChatAction(
+    func=lambda e: (e.user_added or e.user_joined) and e.user_id != bot.me.id))
+async def greet(event: events.ChatAction.Event):
+    await event.respond('Шалом!')
